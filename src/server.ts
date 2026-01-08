66c1a19127099af313fa31ade75e0812e14c5a14
maharonline <hamzaahmad3006@gmail.com>
2026-01-08 18:06:40 +0500

    feat: updated cors

--- src/server.ts
+++ src/server.ts
@@ -27,11 +27,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: [
-        process.env.FRONTEND_URL,
-        'http://localhost:3000',
-        'https://project-management-system-two.vercel.app'
-    ].filter((url): url is string => Boolean(url)),
+    origin: "*",
     credentials: true
 }));
 app.use(express.json());
