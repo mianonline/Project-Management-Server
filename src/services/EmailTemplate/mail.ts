b12008bb59f5689386781cb2c9edb73258f1da25
maharonline <hamzaahmad3006@gmail.com>
2026-01-09 12:14:20 +0500

    debug: revert to service gmail and increase timeout to 30s

--- src/services/EmailTemplate/mail.ts
+++ src/services/EmailTemplate/mail.ts
@@ -1,20 +1,14 @@
 import nodemailer from 'nodemailer'
 
 export const mailTransport = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    pool: true,   // reuse connections
+    service: "gmail",
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
-    connectionTimeout: 15000, // Increase to 15s
-    greetingTimeout: 15000,
-    socketTimeout: 15000,
-    logger: true, // Log to console
-    debug: true,  // Include SMTP traffic in logs
-    tls: {
-        rejectUnauthorized: false // Helps in some cloud environments
-    }
+    connectionTimeout: 30000, // Increase to 30s
+    greetingTimeout: 30000,
+    socketTimeout: 30000,
+    logger: true,
+    debug: true,
 });
